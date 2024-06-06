fc06d127a0eb6c1182e2687e696f84fc171a1e39
HarshulKachariya <108606007+HarshulKachariya@users.noreply.github.com>
2024-06-06 16:09:13 +0530

    "Updated login page layout and styling"

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,14 +3,20 @@ import React from "react";
 
 const Login = () => {
   return (
-    <dialog className="flex h-screen w-screen justify-center items-center">
-      <button
-        onClick={() => changeAuthStatus(false)}
-        className="border-2 border-white p-5"
+    <div className="fixed inset-0 flex items-center justify-center z-50">
+      <div className="fixed inset-0 bg-black opacity-50"></div>
+      <dialog
+        className="relative flex flex-col items-center justify-center p-10 max-w-full max-h-full w-[90%] h-auto md:w-[700px] md:h-[500px] bg-zinc-800 shadow-lg rounded-lg text-white"
+        open
       >
-        Login
-      </button>
-    </dialog>
+        <button
+          onClick={() => changeAuthStatus(false)}
+          className="border-2 border-white p-3 mt-5"
+        >
+          Login
+        </button>
+      </dialog>
+    </div>
   );
 };
 
